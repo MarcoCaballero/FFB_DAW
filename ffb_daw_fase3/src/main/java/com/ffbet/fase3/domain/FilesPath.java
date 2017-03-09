ae7a0b67656a5309f50a63a34a0fc6070665bfbf
elliotAlderson <marco.caballero.d@gmail.com>
2017-03-09 18:54:26 +0100

    Handle photoUpload and download

---
+++ ffb_daw_fase3/src/main/java/com/ffbet/fase3/domain/FilesPath.java
@@ -0,0 +1,26 @@
+package com.ffbet.fase3.domain;
+
+public enum FilesPath {
+
+	
+	FILES_AVATARS("src/main/resources/static/files/avatars"),
+	FILES_TEAMS_COVER("src/main/resources/static/files/teams/covers"),
+	FILES_TEAMS_LOGO("src/main/resources/static/files/teams/logos");
+	
+	private final String text;
+		
+	/**
+     * @param text
+     */
+    private FilesPath(final String text) {
+        this.text = text;
+    }
+
+    /* (non-Javadoc)
+     * @see java.lang.Enum#toString()
+     */
+    @Override
+    public String toString() {
+        return text;
+    }
+}
