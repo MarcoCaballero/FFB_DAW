332649bacf4130056e7e828c18e173e4fec35c9d
elliotAlderson <marco.caballero.d@gmail.com>
2017-03-11 17:33:52 +0100

    Cambios iniciales cookies

--- ffb_daw_fase3/src/main/java/com/ffbet/fase3/controllers/UserSportsBetController.java
+++ ffb_daw_fase3/src/main/java/com/ffbet/fase3/controllers/UserSportsBetController.java
@@ -3,13 +3,19 @@
  */
 package com.ffbet.fase3.controllers;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
 import org.apache.catalina.servlet4preview.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.util.WebUtils;
 
 import com.ffbet.fase3.domain.TemplatesPath;
+import com.ffbet.fase3.domain.User;
 import com.ffbet.fase3.repositories.UserRepository;
 import com.ffbet.fase3.security.UserAuthComponent;
 
@@ -26,31 +32,38 @@ public class UserSportsBetController extends RedirectController {
 	@Autowired
 	UserRepository userRepo;
 	private boolean showsUserMenu = false;
+	private boolean isFirstCome = true;
 
 	@GetMapping(value = { "/user-sportsBet", "/user-sportsBet/" })
-	public String getTemplate(HttpServletRequest request, Model model) {
-		
-		if(userComp.isLoggedUser()){
-			showsUserMenu = true;
+	public String getTemplate(HttpServletRequest request, Model model, HttpSession session, HttpServletResponse response) {
 
-			model.addAttribute("user",  userRepo.findByEmail(userComp.getLoggedUser().getEmail()));
-			if(!userComp.getLoggedUser().isPhotoSelected()){
-				//model.addAttribute("isMen", userComp.getLoggedUser().isMen());
-			}else{
-				//Use image controller
+		if (userComp.isLoggedUser()) {
+			showsUserMenu = true;
+			User updatedUser = userRepo.findByEmail(userComp.getLoggedUser().getEmail());
+			model.addAttribute("user", updatedUser);
+			
+			if(isFirstCome){
+				response.addCookie(new Cookie("ffbetSession", "UserFFB" + updatedUser.getId()));
+				isFirstCome = !isFirstCome;
 			}
-		}else{
+			System.out.println("ffbetSession " + WebUtils.getCookie(request, "ffbetSession"));
+			
+			if (!userComp.getLoggedUser().isPhotoSelected()) {
+				// model.addAttribute("isMen",
+				// userComp.getLoggedUser().isMen());
+			} else {
+				// Use image controller
+			}
+		} else {
 			showsUserMenu = false;
 		}
-		
-		
-		model.addAttribute("isUsermenuActive", showsUserMenu);
 
+		model.addAttribute("isUsermenuActive", showsUserMenu);
 
 		// Checks the URLs with "/*" pattern
 		// Delete the last bar if the requested URL is like "/*/"
-		String response = check_url(request, TemplatesPath.USER_SPORT_BET.toString());
-		return response;
+		String responseTemplate = check_url(request, TemplatesPath.USER_SPORT_BET.toString());
+		return responseTemplate;
 
 	}
 
