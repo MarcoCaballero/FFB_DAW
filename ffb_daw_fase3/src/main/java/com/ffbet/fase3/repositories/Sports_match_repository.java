3651e3f756d6832c48258294adf4e242605c18d2
peloxoo <peloxoo@gmail.com>
2017-03-11 17:42:25 +0100

    Resuelto mostrar partidos acabados
    Y que se marquen como acabados. Cambiada la tabla Match con una nueva
    columna booleana, isFinished.

--- ffb_daw_fase3/src/main/java/com/ffbet/fase3/repositories/Sports_match_repository.java
+++ ffb_daw_fase3/src/main/java/com/ffbet/fase3/repositories/Sports_match_repository.java
@@ -3,9 +3,14 @@ package com.ffbet.fase3.repositories;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
 import com.ffbet.fase3.domain.SportsMatch;
 
 public interface Sports_match_repository extends JpaRepository<SportsMatch, Long>{
 	
 	List<SportsMatch> findByType(String type);
+	
 }
