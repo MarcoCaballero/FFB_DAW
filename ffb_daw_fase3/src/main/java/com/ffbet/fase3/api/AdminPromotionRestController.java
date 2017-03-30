4582904d747bcf1905e4a8dfc837c489a76357e5
peloxoo <peloxoo@gmail.com>
2017-03-30 16:12:46 +0200

    Working promotion rest controller

--- ffb_daw_fase3/src/main/java/com/ffbet/fase3/api/AdminPromotionRestController.java
+++ ffb_daw_fase3/src/main/java/com/ffbet/fase3/api/AdminPromotionRestController.java
@@ -1,131 +1,51 @@
 package com.ffbet.fase3.api;
 
-import java.util.Collection;
-
-import javax.servlet.http.HttpServletRequest;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ffbet.fase3.domain.Promotion;
 import com.ffbet.fase3.services.PromoService;
 
 @RestController
+@RequestMapping("/api/promos")
 public class AdminPromotionRestController {
-
+	
 	@Autowired
 	private PromoService promoService;
-
-	@GetMapping(value = { "/admin-promotions/{id}", "/admin-promotions/{id}/" })
-	public ResponseEntity<Promotion> getPromotionByID(HttpServletRequest request, @PathVariable long id) {
-
-		Promotion promotion = promoService.findOne(id);
-
-		if (promotion != null) {
-			return new ResponseEntity<>(promotion, HttpStatus.OK);
-		} else {
-			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-		}
-
-	}
-
-	/**
-	 * API REST Method {@linkplain deletePromotionByID()} delete the correct
-	 * promotion from repository, and returns the promotion selected [selected
-	 * by id] properties.
-	 *
-	 *
-	 * @param request
-	 * @param model
-	 * @param id
-	 * @return ResponseEntity<Promotion>
-	 */
-	@RequestMapping(value = { "/admin-promotions/{id}", "/admin-promotions/{id}/" }, method = RequestMethod.DELETE)
-	public ResponseEntity<Promotion> deletePromotionByID(HttpServletRequest request, Model model,
-			@PathVariable long id) {
-
-		Promotion promotion = promoService.findOne(id);
-		promoService.delete(id);
-
-		if (promotion != null) {
-			return new ResponseEntity<>(promotion, HttpStatus.OK);
-		} else {
-			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-		}
-
-	}
-
-	/**
-	 * API REST Method {@linkplain updatePromotionByID()} update the correct
-	 * promotion from repository, and returns the promotion selected [selected
-	 * by id] properties.
-	 *
-	 *
-	 * @param request
-	 * @param model
-	 * @param id
-	 * @return ResponseEntity<Promotion>
-	 */
-	@RequestMapping(value = { "/admin-promotions/update/{id}",
-			"/admin-promotions/update/{id}/" }, method = RequestMethod.PUT)
-	public ResponseEntity<Promotion> updatePromotionByID(HttpServletRequest request, Model model, @PathVariable long id,
-			@RequestBody Promotion updatePromotion) {
-
-		Promotion promotion = promoService.findOne(id);
-
-		if (promotion != null) {
-			updatePromotion.setId(id);
-			promoService.save(updatePromotion);
-			return new ResponseEntity<>(promotion, HttpStatus.OK);
-		} else {
-			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-		}
-	}
-
-	/**
-	 * API REST Method {@linkplain deletePromotionByID()} add a new promotion to
-	 * the repository, and returns the promotion created [selected by id]
-	 * properties.
-	 *
-	 *
-	 * @param request
-	 * @param model
-	 * @param id
-	 * @return ResponseEntity<Promotion>
-	 */
-	@RequestMapping(value = { "/admin-promotions/new", "/admin-promotions/new/" }, method = RequestMethod.POST)
-	@ResponseStatus(HttpStatus.CREATED)
-	public Promotion addPromotion(HttpServletRequest request, @RequestBody Promotion newPromotion) {
-
-		promoService.save(newPromotion);
-
-		return newPromotion;
-	}
-
-	/**
-	 * API REST Method {@linkplain getPromotions()} get all existing promotions
-	 * from the repository, and returns a collections of them. properties.
-	 *
-	 *
-	 * @param request
-	 * @param model
-	 * @param id
-	 * @return Collection<Promotion>
-	 */
-	@RequestMapping(value = { "/admin-promotions/promotions",
-			"/admin-promotions/promotions/" }, method = RequestMethod.GET)
-	public Collection<Promotion> getPromotions() {
-		System.out.println("as");
+	
+	// mostrar promociones
+	@RequestMapping(value = "/getPromos", method = RequestMethod.GET)
+	public List<Promotion> findAll(){
 		return promoService.findAll();
 	}
-
+	
+	// añadir promoción
+	@RequestMapping(value = "/newPromo", method = RequestMethod.PUT)
+	public Promotion newPromotion(Promotion promo){
+		promoService.save(promo);
+		
+		return promo;
+	}
+	
+	// eliminar promoción por id
+	@RequestMapping(value = "/deletePromo/{id}", method = RequestMethod.DELETE)
+	public ResponseEntity<Promotion> deletePromotion(@PathVariable long id){
+		Promotion promo = promoService.findOne(id);
+		
+		if(promo != null){
+			promoService.delete(id);
+			
+			return new ResponseEntity<>(promo, HttpStatus.OK);
+		}else{
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+	}
+	
 }
--- ffb_daw_fase3/src/main/java/com/ffbet/fase3/api/AdminTeamRestController.java
+++ ffb_daw_fase3/src/main/java/com/ffbet/fase3/api/AdminTeamRestController.java
@@ -1,13 +1,20 @@
 package com.ffbet.fase3.api;
 
-import java.util.Collection;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.ffbet.fase3.domain.EgamesTeam;
 import com.ffbet.fase3.domain.SportTeam;
+import com.ffbet.fase3.domain.Team;
 import com.ffbet.fase3.services.TeamService;
 
 @RestController
@@ -15,12 +22,89 @@ import com.ffbet.fase3.services.TeamService;
 public class AdminTeamRestController {
 	
 	@Autowired
-	private TeamService service;
+	private TeamService teamService;
 	
-	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public Collection<SportTeam> getTeams(){
-		return service.findAllSportsTeams();
+	// mostrar equipos
+	@RequestMapping(value = "/getTeams", method = RequestMethod.GET)
+	public List<Team> getTeams(){
+		return teamService.findAllTeams();
 	}
 	
+	@RequestMapping(value = "/getSportTeams", method = RequestMethod.GET)
+	public List<SportTeam> getSportTeams(){
+		return teamService.findAllSportsTeams();
+	}
+	
+	@RequestMapping(value = "/getEgamesTeams", method = RequestMethod.GET)
+	public List<EgamesTeam> getEgamesTeams(){
+		return teamService.findAllEgamesTeams();
+	}
+	
+	//añadir equipos
+	@RequestMapping(value = "/newSportTeam", method = RequestMethod.POST)
+	@ResponseStatus(HttpStatus.CREATED)
+	public SportTeam newSportTeam(@RequestBody SportTeam sportTeam){
+		
+		teamService.saveSportTeam(sportTeam);
+		
+		return sportTeam;
+	}
+	
+	@RequestMapping(value = "/newEgamesTeam", method = RequestMethod.POST)
+	@ResponseStatus(HttpStatus.CREATED)
+	public EgamesTeam newEgamesTeam(@RequestBody EgamesTeam egamesTeam){
+		
+		teamService.saveEgamesTeam(egamesTeam);
+		
+		return egamesTeam;
+	}
+	
+	// actuaizar equipos ¿por id o por nombre?
+	@RequestMapping(value = "/updateSportTeam/{id}", method = RequestMethod.PUT)
+	public ResponseEntity<SportTeam> updateSportTeam(@PathVariable long id, @RequestBody SportTeam updatedTeam){
+		
+		SportTeam team = teamService.findOneSportTeam(id);
+		
+		if(team != null){
+			updatedTeam.setId(team.getId());
+			teamService.saveSportTeam(updatedTeam);
+			
+			return new ResponseEntity<>(updatedTeam, HttpStatus.OK);
+		}else{
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+		
+	}
+	
+	@RequestMapping(value = "/updateEgamesTeam/{name}", method = RequestMethod.PUT)
+	public ResponseEntity<EgamesTeam> updateEgamesTeam(@PathVariable String name, @RequestBody EgamesTeam updatedTeam){
+		
+		EgamesTeam team = teamService.findByNameEgames(name);
+		
+		if(team != null){
+			updatedTeam.setId(team.getId());
+			teamService.saveEgamesTeam(updatedTeam);
+			
+			return new ResponseEntity<>(updatedTeam, HttpStatus.OK);
+		}else{
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+		
+	}
+	
+	// borrar equipos
+	@RequestMapping(value = "/deleteTeam/{id}", method = RequestMethod.DELETE)
+	public ResponseEntity<Team> deleteTeam(@PathVariable long id){
+		
+		Team team = teamService.findOneTeam(id);
+		
+		if (team != null) {
+			teamService.delete(id);
+			return new ResponseEntity<>(team, HttpStatus.OK);
+		} else {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+		
+	}
 
 }
