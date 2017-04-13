b39caf2ce509a61cdcef9b4eab22db50f01065da
elliotAlderson <marco.caballero.d@gmail.com>
2017-04-13 05:12:10 +0200

    Adding ngx-tabs

--- angular-client-spa/src/app/user/shared/home.component.ts
+++ angular-client-spa/src/app/user/shared/home.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { TabsetComponent } from 'ngx-bootstrap';
 
 @Component({
     moduleId: module.id,
@@ -11,7 +12,25 @@ export class HomeComponent implements OnInit {
     // Public fields
     title = 'HOME';
 
+    @ViewChild('staticTabs') staticTabs: TabsetComponent;
+
+
+
     constructor() { }
 
     ngOnInit() { }
+
+    public alertMe(): void {
+        setTimeout(function (): void {
+            alert('You\'ve selected the alert tab!');
+        });
+    }
+
+    selectTab(tab_id: number) {
+        this.staticTabs.tabs[tab_id].active = true;
+    }
+
+    disableEnable() {
+        this.staticTabs.tabs[2].disabled = !this.staticTabs.tabs[2].disabled
+    }
 }
