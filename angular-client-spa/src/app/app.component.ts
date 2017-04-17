5ac4cf7e25222abeea123c9a3a371eac063fb4c0
elliotAlderson <marco.caballero.d@gmail.com>
2017-04-17 02:35:30 +0200

    User layout ready
    We need auxiliar forms.

--- angular-client-spa/src/app/app.component.ts
+++ angular-client-spa/src/app/app.component.ts
@@ -4,7 +4,7 @@ import { Component, OnInit } from '@angular/core';
     moduleId: module.id,
     selector: 'ffbcomp-root',
     template: `
-    <ffbcomp-admin></ffbcomp-admin>
+    <ffbcomp-user></ffbcomp-user>
   `
 })
 
