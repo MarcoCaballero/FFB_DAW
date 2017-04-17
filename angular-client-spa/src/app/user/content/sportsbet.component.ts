b77721b497c4874ac30890b8d22314ff916d20e1
elliotAlderson <marco.caballero.d@gmail.com>
2017-04-17 15:40:47 +0200

    All files passs linting

--- angular-client-spa/src/app/user/content/sportsbet.component.ts
+++ angular-client-spa/src/app/user/content/sportsbet.component.ts
@@ -8,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 
 export class SportsbetComponent implements OnInit {
+
     title = 'SportsBet';
+
     constructor() { }
 
     ngOnInit() { }
 
 }
+
