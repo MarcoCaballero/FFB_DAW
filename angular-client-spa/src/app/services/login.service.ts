8c622bfaf83401972f987f84432457d10a59079a
peloxoo <peloxoo@gmail.com>
2017-04-20 02:01:57 +0200

    Login and logout completely working

--- angular-client-spa/src/app/services/login.service.ts
+++ angular-client-spa/src/app/services/login.service.ts
@@ -1,73 +1,68 @@
-import { Injectable } from '@angular/core';
+import { Injectable, OnDestroy } from '@angular/core';
 import { Http, Headers, RequestOptions } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/Rx';
 
-export interface User {
-    id?: number;
-    email: string;
-    roles: string[];
-}
+import { User } from '../model/user.model';
 
+import { UserService } from './user.service';
 
 @Injectable()
-export class LoginService {
+export class LoginService implements OnDestroy {
+
     isLogged = false;
     isAdmin = false;
     user: User;
+    credentials: string;
 
-    constructor(private http: Http) {
-        this.reqIsLogged();
+    ngOnDestroy() {
+        console.log('localStorage called from ngOnDestroy');
+        localStorage.clear();
     }
 
-    reqIsLogged() {
-        let headers = new Headers({
-            'X-Requested-With': 'XMLHttpRequest'
-        });
-
-        let options = new RequestOptions({ headers });
-
-        this.http.get('http://127.0.0.1:8080/api/logIn', options).subscribe(
-            response => this.processLogInResponse(response),
-            error => {
-                if (error.status !== 401) {
-                    console.error('Error when asking if logged: ' +
-                        JSON.stringify(error));
-                }
-            }
-        );
-    }
-
-    private processLogInResponse(response) {
-        this.isLogged = true;
-        this.user = response.json();
-        this.isAdmin = this.user.roles.indexOf('ROLE_ADMIN') !== -1;
-    }
+    constructor(
+        private http: Http,
+        private userService: UserService) { }
 
     logIn(username: string, password: string) {
-        let userPass = username + ':' + password;
+        this.credentials = utf8_to_b64(username + ':' + password);
         let headers = new Headers({
-            'Authorization': 'Basic ' + utf8_to_b64(userPass),
-            'X-Requested-With': 'XMLHttpRequest'
+            'Authorization': 'Basic ' + this.credentials
         });
         let options = new RequestOptions({ headers });
         return this.http.get('http://127.0.0.1:8080/api/logIn', options).map(
             response => {
-                this.processLogInResponse(response);
+                let id = response.json().id;
+                localStorage.setItem('credentials', this.credentials);
+                localStorage.setItem('id', String(id));
+                this.userService.setHeaders(this.credentials);
+                this.user = response.json();
+                this.isAdmin = this.user.roles.indexOf('ROLE_ADMIN') !== -1;
+                localStorage.setItem('user', username);
+                localStorage.setItem('password', password);
+                this.isLogged = true;
                 return this.user;
-            },
-            error => {
-                console.error('Error when asking if logged: ' + JSON.stringify(error));
-            }
-        );
+            })
+            .catch(error => Observable.throw('Server error'));
     }
 
     logOut() {
-        return this.http.get('http://127.0.0.1:8080/api/logOut').map(
+        let headers = new Headers({
+            'Authorization': 'Basic ' + this.credentials
+        });
+        let options = new RequestOptions({ headers });
+        return this.http.get('http://127.0.0.1:8080/api/logOut', options).map(
             response => {
+                localStorage.clear();
                 this.isLogged = false;
                 this.isAdmin = false;
                 return response;
             }
         );
+    }
+
+    checkCredentials() {
+        return (localStorage.getItem('user') !== null);
     }
 }
 
