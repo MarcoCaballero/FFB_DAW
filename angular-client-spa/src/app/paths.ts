d6f5f3ae718398b03d45784910bf2ad1a33c1930
elliotAlderson <marco.caballero.d@gmail.com>
2017-04-21 20:00:29 +0200

    Build done on target mvn package

--- angular-client-spa/src/app/paths.ts
+++ angular-client-spa/src/app/paths.ts
@@ -1,5 +1,6 @@
 // Base URL
-export const baseUrl = 'https://localhost:8080/api/';
+export const baseUrl = 'http://127.0.0.1:8080/api/';
 
 // Login Module URL'S
 export const loginUrl = baseUrl + 'logIn';
+export const logoutUrl = baseUrl + 'logOut';
