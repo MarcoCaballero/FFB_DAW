d2a326208fc75c77e181494948fa0098f8eb62f8
elliotAlderson <marco.caballero.d@gmail.com>
2017-04-23 16:11:51 +0200

    Handling beforeunload by @HostListener

--- angular-client-spa/src/app/app.component.ts
+++ angular-client-spa/src/app/app.component.ts
@@ -1,25 +1,35 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit, OnDestroy, HostListener } from '@angular/core';
 
 import { AuthService } from './services/auth.service';
 
 @Component({
     moduleId: module.id,
     selector: 'ffbcomp-root',
+    host: { 'window:beforeunload': 'clearAuth' },
     template: `
     <router-outlet></router-outlet>
   `
 })
 
 export class AppComponent implements OnInit, OnDestroy {
+    @HostListener('window:beforeunload') clearAuth() {
+        console.log('Cleaning credential authorization.....');
+        this.authService.clear();
+        localStorage.clear();
+    }
+
     constructor(
         private authService: AuthService
     ) { }
+
 
     ngOnInit() {
         this.authService.reloadAuth(); // Reload auth.service fields from localStorage
     }
 
     ngOnDestroy() {
+        console.log('Cleaning credential authorization.....');
+        this.authService.clear();
         localStorage.clear();
     }
 
